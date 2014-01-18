8333769158b3a2298ffac45dacce299090d8c443
Jason Laxdal <jason@onthecity.org>
2014-01-17 22:52:57 -0800

    Base rails 4 app, mysql db, haml views.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Start::Application.config.secret_key_base = 'c374d76968e09abd2c42e4c536ac2bb40e4ba80bd7ac20f76fa20ca434cd82338008b2eff43a60d1dae45b618896052bcd4d2aba0d5907da69b153f93afdcf35'
